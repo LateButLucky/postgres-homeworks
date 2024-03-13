b4597f6b420bfcb1390d22449e4176e30bc9b2fc
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-03-13 22:07:46 +0300

    'скопированный код из postgress для создания таблиц'

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+
+""" Создание таблицы employees. """
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date varchar(20) NOT NULL,
+	notes text
+);
+
+""" Создание таблицы customers. """
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+""" Создание таблицы orders. """
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date varchar(20) NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
